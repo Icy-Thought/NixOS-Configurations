e7824f1b6934bb836b2ceefbc6c4c84ca5c48633
Gilganyx <gilganyx@gmail.com>
2021-05-18 12:57:28 +0200

    testing zathura.nix

--- home/zathura.nix
+++ home/zathura.nix
@@ -3,50 +3,46 @@
   programs.zathura = {
     enable = true;
     settings = {
-
-      # Font
-      set font = "Iosevka Bold 10";
+      font = "Iosevka Bold 10";
+      selection-clipboard = "clipboard";
+      first-page-column = "1:1";
+      adjust-open = width;
+      window-title-basename = true;
 
       # Base16 Gruvbox - morhetz
-      set default-bg   = "#262626";
-      set default-fg   = "#ebdbb2";
+      default-bg   = "#262626";
+      default-fg   = "#ebdbb2";
       
-      set statusbar-fg = "#ebdbb2";
-      set statusbar-bg = "#262626";
+      # Status + Input bar:
+      statusbar-fg = "#ebdbb2";
+      statusbar-bg = "#262626";
+      inputbar-bg  = "#262626";
+      inputbar-fg  = "#ebdbb2";
       
-      set inputbar-bg  = "#262626";
-      set inputbar-fg  = "#ebdbb2";
+      # Notification:
+      notification-bg = "#262626";
+      notification-fg = "#458588";
+      notification-error-bg = "#262626";
+      notification-error-fg = "#cc241d";
+      notification-warning-bg = "#262626";
+      notification-warning-fg = "#d79921";
       
-      set notification-error-bg  = "#262626";
-      set notification-error-fg  = "#cc241d";
+      # Highlight:
+      highlight-color         = "#262626";
+      highlight-active-color  = "#ebdbb2";
       
-      set notification-warning-bg  = "#262626";
-      set notification-warning-fg  = "#d79921";
+      # Completion:
+      completion-highlight-fg  = "#4e4e4e";
+      completion-highlight-bg  = "#87afaf";
+      completion-bg  = "#4e4e4e";
+      completion-fg  = "#ebdbb2";
       
-      set highlight-color         = "#262626";
-      set highlight-active-color  = "#ebdbb2";
-      
-      set completion-highlight-fg  = "#4e4e4e";
-      set completion-highlight-bg  = "#87afaf";
-      
-      set completion-bg  = "#4e4e4e";
-      set completion-fg  = "#ebdbb2";
-      
-      set notification-bg = "#262626";
-      set notification-fg = "#458588";
-      
-      set recolor-lightcolor  = "#262626";
-      set recolor-darkcolor   = "#ebdbb2";
-      
-      set recolor          = "true";
-      set recolor-keephue  = "false";
-      
-      # General Settings
-      set selection-clipboard    = "clipboard";
-      set first-page-column      = "1:1";
-      set adjust-open            = width;
-      set window-title-basename  = true;
+      # Force recolor:
+      recolor-lightcolor  = "#262626";
+      recolor-darkcolor   = "#ebdbb2";
+      recolor          = "true";
+      recolor-keephue  = "false";
     };
-  };
 
+  };
 }
