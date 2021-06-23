ddb6179f22d7ad551be812e4622d5b4f9d9dd810
Icy-Thought <gilganyx@pm.me>
2021-06-23 06:24:00 +0200

    Container.nix: added missing semicolon

--- hosts/ThinkPad/containers.nix
+++ hosts/ThinkPad/containers.nix
@@ -1,14 +1,13 @@
 { config, pkgs, lib, ... }:
 {
 
-  
   virtualisation = {
     libvirtd = {
       enable = true;
       qemuVerbatimConfig = ''
         user = "sirius"
       '';
-    }
+    };
   };
 
   systemd.nspawn."Arch" = {
@@ -20,11 +19,13 @@
       Hostname = "Arch";
       SystemCallFilter = "modify_ldt";
     };
+
     filesConfig = {
       Bind = [ "/home/sirius/.container-arch:/home/sirius" "/run/user/1000/wayland-1" "/tmp/.X11-unix/X0" "/tank" "/run/user/1000/pulse/native" "/dev/dri" "/dev/shm" ];
       BindReadOnly = [ "/home/sirius:/mnt/sirius" ];
       Volatile = false;
     };
+
     networkConfig = {
       Private = true;
       VirtualEthernet = true;
@@ -33,7 +34,6 @@
   };
 
   systemd.services."systemd-nspawn@".serviceConfig = {
-    # Vulkan support
     DeviceAllow = [
       "char-drm rwx"
       "/dev/dri/renderD128"
